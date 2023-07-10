a39eaa7eba471656047f762f36a751554f182f4e
cassiokamida <cassio.kida@hotmail.com>
2023-07-10 16:53:27 -0300

    creating the terrain for components, Context and Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
+import { Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+
+  // <div className="meals">
+  //   <span className="logo">TRYBE</span>
+  //   <object
+  //     className="rocksGlass"
+  //     type="image/svg+xml"
+  //     data={ rockGlass }
+  //   >
+  //     Glass
+  //   </object>
+  // </div>
+
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/meals" component={ Foods } />
+      <Route path="/drinks" component={ Drinks } />
+      {/* <Route path="/meals/:id" component={ RecipeDetails } />
+      <Route path="/drinks/:id" component={ DrinkDetails } />
+      <Route path="/meals/:id/in-progress" component={ FoodInProgress } />
+      <Route path="/drinks/:id/in-progress" component={ DrinkInProgress } /> */}
+      <Route path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+    </Switch>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import RecipeProvider from './context/RecipeContext';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<App />);
-// Login
+  .render(
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>,
+  );
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
