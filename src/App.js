a51a7d6228f1dff813ef599c6d25d1297a2c38cd
Rômulo-Reis <romuloreis06@gmail.com>
2023-07-10 17:01:07 -0300

    Add BroserRoute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, browseroute } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 
