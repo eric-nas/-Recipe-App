7c0ff3e5c37e8411585b357fe3b1874183d76a4d
Jonathan Siqueira <jonathan.tosi@gmail.com>
2023-07-11 14:22:13 -0300

    Feat: Create Login page (02 ao 06)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 // import rockGlass from './images/rockGlass.svg';
-import { Switch, Route, browseroute } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/Login';
 
 function App() {
   return (
-
-  // <div className="meals">
-  //   <span className="logo">TRYBE</span>
-  //   <object
-  //     className="rocksGlass"
-  //     type="image/svg+xml"
-  //     data={ rockGlass }
-  //   >
-  //     Glass
-  //   </object>
-  // </div>
-
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route path="/meals" component={ Foods } />
-      <Route path="/drinks" component={ Drinks } />
-      {/* <Route path="/meals/:id" component={ RecipeDetails } />
+    <main>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        {/* <Route path="/meals" component={ Foods } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/meals/:id" component={ RecipeDetails } />
       <Route path="/drinks/:id" component={ DrinkDetails } />
       <Route path="/meals/:id/in-progress" component={ FoodInProgress } />
-      <Route path="/drinks/:id/in-progress" component={ DrinkInProgress } /> */}
-      <Route path="/profile" component={ Profile } />
-      <Route path="/done-recipes" component={ DoneRecipes } />
-      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-    </Switch>
-
+      <Route path="/drinks/:id/in-progress" component={ DrinkInProgress } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } /> */}
+      </Switch>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import RecipeProvider from './context/RecipeContext';
+import RecipeProvider from './context/RecipeProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <RecipeProvider>
       <BrowserRouter>
-      <App />
-    </BrowserRouter>
+        <App />
+      </BrowserRouter>
     </RecipeProvider>,
+
   );
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
